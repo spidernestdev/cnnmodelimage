fc88fc2f84d3405101ff79cea9ba19f646c97c56
spidernestdev <contactspidernestdev@gmail.com>
2026-03-17 22:34:26 +0530

    fix model loading

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 model_path = os.path.join(BASE_DIR, "models", "security_model.keras")  # ✅ updated
 
-model = tf.keras.models.load_model(model_path)
+model = tf.keras.models.load_model(model_path, compile=False)
 
 @app.get("/")
 def home():
